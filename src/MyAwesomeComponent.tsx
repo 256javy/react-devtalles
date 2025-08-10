4484c7aad9a21cf3e57a7ee90a3d48989d3dc025
256javy <256jv@proton.me>
2025-08-10 15:45:47 -0300

    feat: add variables to MyAwesomeApp

--- src/MyAwesomeComponent.tsx
+++ src/MyAwesomeComponent.tsx
@@ -1,10 +1,28 @@
 
+const firstName: string = 'Javier'
+const lastName: string = 'Villalba'
+
+const favoriteGames: string[] = ['Assassins Creed 2', 'Need For Speed Underground 2', 'Grand Theft Auto: San Andreas', 'Counter Strike 1.6']
+
+const isActive = true
+
+const address = {
+    zipCode: 'ABC123',
+    country: 'Paraguay'
+}
 
 export function MyAwesomeApp() {
+
     return (
         <>
-            <h1>Javier</h1>
-            <h3>Villalba</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+
+            <p>{favoriteGames.join(', ')}</p>
+            <p>{2 + 2}</p>
+            <h1>{isActive}</h1>
+
+            <p>{JSON.stringify(address)}</p>
         </>
     )
 }
