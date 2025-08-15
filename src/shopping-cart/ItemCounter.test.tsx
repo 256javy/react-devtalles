afeef7302d94253a3da767f6f882a4b25fe3a453
256javy <256jv@proton.me>
2025-08-15 20:19:42 -0300

    test: add test with fire events

--- src/shopping-cart/ItemCounter.test.tsx
+++ src/shopping-cart/ItemCounter.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { ItemCounter } from "./ItemCounter";
 
@@ -8,7 +8,7 @@ describe('ItemCounter', () => {
         const testItemText = 'Test item';
 
         render(<ItemCounter itemName={testItemText} />)
-        screen.debug();
+        //screen.debug();
 
         expect(screen.getByText(testItemText)).toBeDefined();
 
@@ -19,9 +19,40 @@ describe('ItemCounter', () => {
         const customQty = 10;
 
         render(<ItemCounter itemName={testItemText} quantity={customQty} />)
-        screen.debug();
+        //screen.debug();
 
         expect(screen.getByText(customQty)).toBeDefined();
 
     });
+
+    test('should increase count when +1 button is pressed', () => {
+        render(<ItemCounter itemName={"TestItem"} quantity={1} />)
+        //screen.debug();
+
+        const [buttonAdd] = screen.getAllByRole('button');
+        fireEvent.click(buttonAdd)
+
+        expect(screen.getByText("2").innerHTML).toBe("2")
+    });
+
+    test('should decrease count when -1 button is pressed', () => {
+        const customQty = 5;
+        render(<ItemCounter itemName={"TestItem"} quantity={customQty} />)
+        //screen.debug();
+
+        const [,buttonDecrease] = screen.getAllByRole('button');
+        fireEvent.click(buttonDecrease)
+
+        expect(Number(screen.getByText(customQty-1).innerHTML)).toBe(customQty-1)
+    });
+
+    test('should not decrease count when -1 button is pressed and quantity is 1', () => {
+        render(<ItemCounter itemName={"TestItem"} quantity={1} />)
+
+        const [,buttonDecrease] = screen.getAllByRole('button');
+        fireEvent.click(buttonDecrease)
+
+        expect(screen.getByText("1").innerHTML).toBe("1")
+    });
+
 });
